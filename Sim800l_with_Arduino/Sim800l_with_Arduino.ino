fc50b34d6f3eef38695d1c52d4107e915b0bceeb
thien29797 <31227943+thien29797@users.noreply.github.com>
2019-05-07 21:07:12 +0700

    Update Sim800l_with_Arduino.ino

--- Sim800l_with_Arduino/Sim800l_with_Arduino.ino
+++ Sim800l_with_Arduino/Sim800l_with_Arduino.ino
@@ -15,48 +15,57 @@ void setup() {
   Serial.begin(9600);
   delay(500);
   sim.begin(9600);
-  delay(500);
+//  SendMessage("Test");
+//  delay(1000);
+//  callNumber();
   
 }
 
 void loop() {
-    val = digitalRead(sensor);
-    
-      if (val == HIGH) {      
-        digitalWrite(led, HIGH);
-        delay(100);              
-        
-        if (state == LOW) {
-//          Serial.println("Motion detected!");
-//          String data = "Motion detected!";
-//          SendMessage(data); 
-          callNumber();
-          state = HIGH;       
-        }
-      } 
-      else {
-          digitalWrite(led, LOW);
-          delay(200);             
-          
-          if (state == HIGH){
-//            Serial.println("Motion stopped!");
-//            String data = "Motion stopped!";
-//            SendMessage(data);
-            state = LOW;       
-        }
-      }
-    
+//    val = digitalRead(sensor);
+//    
+//      if (val == HIGH) {      
+//        digitalWrite(led, HIGH);
+//        delay(100);              
+//        
+//        if (state == LOW) {
+////          Serial.println("Motion detected!");
+////          String data = "Motion detected!";
+////          SendMessage(data); 
+//          callNumber();
+//          state = HIGH;       
+//        }
+//      } 
+//      else {
+//          digitalWrite(led, LOW);
+//          delay(200);             
+//          
+//          if (state == HIGH){
+////            Serial.println("Motion stopped!");
+////            String data = "Motion stopped!";
+////            SendMessage(data);
+//            state = LOW;       
+//        }
+//      }
+//    
+  delay(1000);
+  SendMessage("test");
+  Serial.println("SMS sended");
+  delay(2000);
+//  callNumber();
+//  Serial.println("called");
+//  delay(2000);
 }
 
 void SendMessage(String data)
 {
   sim.println("AT+CMGF=1");
-  delay(500);
+  delay(2000);
   sim.println("AT+CMGS=\"" + number + "\"\r");
-  delay(500);
+  delay(2000);
   String SMS = data;
   sim.println(SMS);
-  delay(100);
+  delay(1000);
   sim.println((char)26);
   delay(1000);
 }
